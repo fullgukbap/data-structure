b22ed08d0717deeade8080f53530c455787acc4c
fullgukbap <2hanjunbum6@gmail.com>
2024-11-22 10:11:39 +0900

    feat: sort 테스트 파일 작성

--- sort/sort_test.go
+++ sort/sort_test.go
@@ -17,3 +17,48 @@ func TestQuickSort(t *testing.T) {
 	QuickSort(arr)
 	assert.True(t, IsSorted(arr), arr)
 }
+
+func TestMergeSort(t *testing.T) {
+	arr := make([]int, 1000)
+	for i := 0; i < 10; i++ {
+		arr[i] = rand.Intn(100)
+	}
+
+	assert.False(t, IsSorted(arr))
+	sorted := MergeSort(arr)
+	assert.True(t, IsSorted(sorted), arr)
+}
+
+func BenchmarkQuickSort(b *testing.B) {
+	arr := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		arr[i] = rand.Intn(b.N)
+	}
+
+	QuickSort(arr)
+}
+
+func BenchmarkMergeSort(b *testing.B) {
+	arr := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		arr[i] = rand.Intn(b.N)
+	}
+
+	MergeSort(arr)
+}
+
+func BenchmarkBinaryInsertSort(b *testing.B) {
+	arr := make([]int, b.N)
+	for i := 0; i < b.N; i++ {
+		arr = BinaryInsertSort(arr, rand.Intn(100))
+	}
+}
+
+func TestBinaryInsertSort(t *testing.T) {
+	arr := make([]int, 0, 100)
+	for i := 0; i < 100; i++ {
+		arr = BinaryInsertSort(arr, rand.Intn(100))
+	}
+
+	assert.True(t, IsSorted(arr), arr)
+}
