f02560f4afd6d2f276147500ab222e5202b77bcd
fullgukbap <2hanjunbum6@gmail.com>
2024-11-22 10:11:09 +0900

    feat: quick sort 구현

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -6,7 +6,6 @@ func QuickSort[T constraints.Ordered](arr []T) {
 	if len(arr) <= 1 {
 		return
 	}
-
 	idx := patition(arr)
 	QuickSort(arr[:idx])
 	QuickSort(arr[idx+1:])
@@ -19,24 +18,17 @@ func patition[T constraints.Ordered](arr []T) int {
 	pivot := arr[0]
 	i := 1
 	j := len(arr) - 1
-
 	for {
-		// pivot 보다 큰 값 찾아감
 		for i < len(arr) && arr[i] <= pivot {
 			i++
 		}
-
-		// pivot 보다 작은 값 찾아감
 		for j > 0 && arr[j] > pivot {
 			j--
 		}
-
-		// 만약 서로 역전되었다면? -> pivot의 위치
 		if i >= j {
 			arr[0], arr[i-1] = arr[i-1], arr[0]
 			return i - 1
 		}
-
 		arr[i], arr[j] = arr[j], arr[i]
 	}
 }
